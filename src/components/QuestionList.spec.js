fa70b869e07e93a88bee4b40cfdb1db6a805692e
kwiecien <k.d.kwiecinski@gmail.com>
2019-01-26 22:21:42 +0100

    5: add setup and teardown

--- src/components/QuestionList.spec.js
+++ src/components/QuestionList.spec.js
@@ -1,5 +1,25 @@
 describe('The Question List', () => {
-    it('should display a list of items', () => {
+    beforeEach(() => {
+        console.log("Before each!");
+    });
 
+    beforeAll(() => {
+        console.log("Before all!");
+    });
+
+    afterEach(() => {
+        console.log("Before each!");
+    });
+
+    afterAll(() => {
+        console.log("Before all!");
+    });
+
+    it('should display a list of items', () => {
+        expect(40 + 2).toEqual(42);
+    });
+
+    it('should be the meaning of life', () => {
+        expect(40 + 2).toEqual(42);
     });
 });
