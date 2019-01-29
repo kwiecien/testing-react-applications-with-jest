86272b38082397602592a9db6d2cce571180fa8e
kwiecien <k.d.kwiecinski@gmail.com>
2019-01-29 21:57:03 +0100

    8: React Redux Display Testing Demo

--- src/components/__tests__/QuestionDetail.js
+++ src/components/__tests__/QuestionDetail.js
@@ -1,4 +1,6 @@
 import {mapStateToProps, QuestionDetailDisplay} from '../QuestionDetail';
+import renderer from 'react-test-renderer';
+import React from 'react';
 
 describe('The Question Detail Component', () => {
     descibe('The Container Element', () => {
@@ -20,7 +22,18 @@ describe('The Question Detail Component', () => {
         });
     });
 
-    it('should not regress', () => {
-
+    describe("The display element", () => {
+        it('should not regress', () => {
+            const tree = renderer.create(
+                <QuestionDetailDisplay
+                    title="The meaning of life"
+                    body="42"
+                    answer_count={0}
+                    tags={['hitchhiking']}
+                />
+            );
+            expect(tree.toJSON()).toMatchSnapshot();
+        });
     });
+
 });
