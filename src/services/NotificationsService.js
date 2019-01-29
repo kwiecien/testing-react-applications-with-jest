b735c71d1426548b4f0656be0a2172427a39eb5b
kwiecien <k.d.kwiecinski@gmail.com>
2019-01-29 22:21:03 +0100

    8: Building a Stateful React Component

---
+++ src/services/NotificationsService.js
@@ -0,0 +1,9 @@
+import {delay} from "redux-saga";
+
+export default {
+    async getNotifications() {
+        console.warn("REAL NOTIFICATION SERVICE - CONTACTING APIS!");
+        await delay(1000);
+        return {count: 42};
+    }s
+};
